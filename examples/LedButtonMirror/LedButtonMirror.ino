a041fd459e5c35804994b25861fbc2b84456e81c
rpidanny <abhishekmaharjan1993@gmail.com>
2018-06-17 10:17:27 +0200

    added examples

---
+++ examples/LedButtonMirror/LedButtonMirror.ino
@@ -0,0 +1,17 @@
+#include <TM1638.h>
+
+#define LOAD_PIN 7
+#define DIO 11
+#define CLK 13
+
+TM1638 disp(LOAD_PIN, DIO, CLK);
+
+void setup() {
+  disp.begin();
+  disp.setBrightness(5);
+}
+
+void loop() {
+  disp.led(disp.readButton());
+  delay(10);
+}
